068e10ec72f6b6453cd5b39d15544487f93360a7
Nilesh Gule <vn_nilesh@yahoo.com>
2022-10-08 10:34:25 +0800

    Separated readme file with doc for building contianer images

--- README.md
+++ README.md
@@ -17,35 +17,7 @@ Note: I am using a Azure Container Registry (ACR) to publish the container image
 
 ## Steps to build and deploy the application
 
-### Build docker images
-
-The source code for the application is available in the [src](src) folder.
-
-- [TechTalksModel](src/TechTalksModel/) is a .Net Standard library which is used by both the microservices.
-- [TechTalksProducer](src/TechTalksProducer/) is a .Net Core Web API which is used to publish messages to the RabbitMQ cluster.
-- [TechTalksConsumer](src/TechTalksConsumer/) is a .Net Core Web API which is used to consume messages from the RabbitMQ cluster.
-
-To build the docker images, run the following commands:
-
-```Powershell
-
-docker-compose -f docker-compose-acr.yml build
-
-```
-
-This will build the images for both the microservices and tag them with the tag name `azurecontainerapp`. The images will be pushed to the Azure Container Registry (ACR). If you wish to change the container registry feel free to update the `docker-compose-acr.yml` file.
-
-We can verify that the images are created successfully by running the following command:
-
-```Powershell
-
-docker images
-
-```
-
-The output should look like this:
-
-![docker images](/images/docker-images.png)
+Follow the steps mentioned in the [build container images](docs/build-container-images.md) to build the container images for TechTalks Producer and Consumer applications.
 
 ### Push the docker images to the container registry
 
---
+++ powershell/setup-tech-talks-container-app.ps1
@@ -0,0 +1,155 @@
+Param(
+    [parameter(Mandatory = $false)]
+    [string]$subscriptionName = "Microsoft Azure Sponsorship",
+    [parameter(Mandatory = $false)]
+    [string]$resourceGroupName = "azure-container-app-rg",
+    [parameter(Mandatory = $false)]
+    [string]$resourceGroupLocaltion = "eastasia",
+    [parameter(Mandatory = $false)]
+    [string]$environmentName = "aci-dev-env"
+)
+
+# Set Azure subscription name
+Write-Host "Setting Azure subscription to $subscriptionName"  -ForegroundColor Yellow
+az account set --subscription=$subscriptionName
+
+$aksRgExists = az group exists --name $resourceGroupName
+
+Write-Host "$resourceGroupName exists : $aksRgExists"
+
+if ($aksRgExists -eq $false) {
+
+    # Create resource group name
+    Write-Host "Creating resource group $resourceGroupName in region $resourceGroupLocaltion" -ForegroundColor Yellow
+    az group create `
+        --name=$resourceGroupName `
+        --location=$resourceGroupLocaltion `
+        --output=jsonc
+}
+
+# Create Azure Container App environment
+Write-Host "Creating Azure Container App environment named : $environmentName "  -ForegroundColor Yellow
+
+az containerapp env create `
+    --name $environmentName `
+    --resource-group $resourceGroupName `
+    --location $resourceGroupLocaltion
+
+Write-Host "Successfully created Azure Container App environment named : $environmentName "  -ForegroundColor Yellow
+
+#Setup Pub Sub Dapr component
+Write-Host "Creating Dapr Pubsub component "  -ForegroundColor Yellow
+
+az containerapp env dapr-component set `
+    --name $environmentName `
+    --resource-group $resourceGroupName `
+    --dapr-component-name rabbitmq-pubsub `
+    --yaml ../k8s/Dapr-components/rabbitmq-dapr.yaml
+
+Write-Host "Successfully created Dapr Pubsub component "  -ForegroundColor Yellow
+
+# Create Azure Container App for TechTalks Producer application
+Write-Host "Creating Azure Container App for Producer "  -ForegroundColor Yellow
+
+az containerapp create `
+    --environment $environmentName `
+    --resource-group $resourceGroupName `
+    --name techtalks-producer `
+    --image ngacrregistry.azurecr.io/techtalksproducer:azurecontainerapp `
+    --registry-server ngacrregistry.azurecr.io `
+    --target-port 80 `
+    --ingress 'external' `
+    --enable-dapr `
+    --dapr-app-id rabbitmq-producer `
+    --dapr-app-port 80 `
+    --min-replicas 1 `
+    --max-replicas 3
+  
+    
+#--query configuration.ingress.fqdn
+    
+Write-Host "Successfully created Azure Container App for Producer "  -ForegroundColor Yellow
+
+# Create Azure Container App for TechTalks Producer application
+Write-Host "Creating Azure Container App for Consumer "  -ForegroundColor Yellow
+
+az containerapp create `
+    --environment $environmentName `
+    --resource-group $resourceGroupName `
+    --name techtalks-consumer `
+    --image ngacrregistry.azurecr.io/techtalksconsumer:azurecontainerapp `
+    --registry-server ngacrregistry.azurecr.io `
+    --target-port 80 `
+    --ingress 'internal' `
+    --enable-dapr `
+    --dapr-app-id rabbitmq-consumer `
+    --dapr-app-port 80 `
+    --min-replicas 1 `
+  
+    
+#--query configuration.ingress.fqdn
+    
+Write-Host "Successfully created Azure Container App for Consumer "  -ForegroundColor Yellow
+
+##Create a new secret named 'svcbus-connstring' in backend processer container app
+az containerapp secret set `
+    --name techtalks-consumer `
+    --resource-group $resourceGroupName `
+    --secrets "rabbitmq-host=amqp://user:tCUN6UizuwTZ@20.24.98.54:5672/"
+
+# define KEDA autoscaler
+
+az containerapp update `
+    --name techtalks-consumer `
+    --resource-group $resourceGroupName `
+    --min-replicas 1 `
+    --max-replicas 15 `
+    --scale-rule-name "rabbitmq-keda-autoscale" `
+    --scale-rule-type "rabbitmq" `
+    --scale-rule-auth "host=rabbitmq-host" `
+    --scale-rule-metadata "queueName=rabbitmq-consumer-techtalks" `
+    "mode=QueueLength" `
+    "value=50" `
+    "protocol=amqp" `
+    "hostFromEnv=rabbitmq-host"
+
+# ##Query Number & names of Replicas
+# az containerapp replica list `
+#     --name techtalks-consumer `
+#     --resource-group azure-container-app-rg `
+#     --query [].name
+
+#verify deployment
+
+# $LOG_ANALYTICS_WORKSPACE_CLIENT_ID = az containerapp env show `
+#     --name $environmentName `
+#     --resource-group $resourceGroupName `
+#     --query properties.appLogsConfiguration.logAnalyticsConfiguration.customerId `
+#     --out tsv
+
+# az monitor log-analytics query `
+#     --workspace $LOG_ANALYTICS_WORKSPACE_CLIENT_ID `
+#     --analytics-query "ContainerAppConsoleLogs_CL | where ContainerAppName_s == 'tech-talks-producer' | project ContainerAppName_s, Log_s, TimeGenerated" `
+#     --out table
+
+# az monitor log-analytics query `
+#     --workspace 8b0a5149-3858-4a53-b05c-ecb0218b9e9a `
+#     --analytics-query "ContainerAppConsoleLogs_CL | where ContainerAppName_s == 'tech-talks-producer' | project ContainerAppName_s, Log_s, TimeGenerated" `
+#     --out table
+    
+
+# az containerapp env dapr-component remove `
+# -g azure-container-app-rg `
+# --dapr-component-name rabbitmq-pubsub `
+# --name aci-dev-env
+
+# az vm create `
+#     --resource-group azure-container-app-rg `
+#     --name rabbitmq-new `
+#     --admin-username azureuser `
+#     --generate-ssh-keys `
+#     --image bitnami:rabbitmq:rabbitmq:latest `
+#     --plan-name rabbitmq `
+#     --plan-product rabbitmq `
+#     --plan-publisher bitnami `
+#     --public-ip-sku Standard
