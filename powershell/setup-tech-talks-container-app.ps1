e87dfff0110b431c296383c2cfc4b53eaaf3f18e
Nilesh Gule <vn_nilesh@yahoo.com>
2022-10-24 00:51:47 +0800

    updated teh script to be idempotent

--- powershell/setup-tech-talks-container-app.ps1
+++ powershell/setup-tech-talks-container-app.ps1
@@ -51,66 +51,121 @@ else {
     Write-Host "Azure Container App environment named : $environmentName already exists"  -ForegroundColor Yellow
 }
 
-#Setup Pub Sub Dapr component
-Write-Host "Creating Dapr Pubsub component "  -ForegroundColor Yellow
-
-az containerapp env dapr-component set `
+$daprComponent = az containerapp env dapr-component show `
     --name $environmentName `
     --resource-group $resourceGroupName `
     --dapr-component-name rabbitmq-pubsub `
-    --yaml ../config/Dapr-components/rabbitmq-dapr.yaml
+    --query name | ConvertFrom-Json
 
-Write-Host "Successfully created Dapr Pubsub component "  -ForegroundColor Yellow
+$daprComponentExists = $daprComponent.Length -gt 0
 
-# Create Azure Container App for TechTalks Producer application
-Write-Host "Creating Azure Container App for Producer "  -ForegroundColor Yellow
+if ($daprComponentExists -eq $false) {
+    #Setup Pub Sub Dapr component
+    Write-Host "Creating Dapr Pubsub component "  -ForegroundColor Yellow
 
-az containerapp create `
-    --environment $environmentName `
+    az containerapp env dapr-component set `
+        --name $environmentName `
+        --resource-group $resourceGroupName `
+        --dapr-component-name rabbitmq-pubsub `
+        --yaml ../config/Dapr-components/rabbitmq-dapr.yaml
+
+    Write-Host "Successfully created Dapr Pubsub component "  -ForegroundColor Yellow
+
+}
+else {
+    Write-Host "Dapr Pubsub component already exists"  -ForegroundColor Yellow
+}
+
+$producerApp = az containerapp show `
     --resource-group $resourceGroupName `
     --name techtalks-producer `
-    --image ngacrregistry.azurecr.io/techtalksproducer:azurecontainerapp `
-    --registry-server ngacrregistry.azurecr.io `
-    --target-port 80 `
-    --ingress 'external' `
-    --enable-dapr `
-    --dapr-app-id rabbitmq-producer `
-    --dapr-app-port 80 `
-    --min-replicas 1 `
-    --max-replicas 3
+    --query name | ConvertFrom-Json
+
+$producerAppExists = $producerApp.Length -gt 0
+
+if ($producerAppExists -eq $false) {
+
+    # Create Azure Container App for TechTalks Producer application
+    Write-Host "Creating Azure Container App for Producer "  -ForegroundColor Yellow
+
+    az containerapp create `
+        --environment $environmentName `
+        --resource-group $resourceGroupName `
+        --name techtalks-producer `
+        --image ngacrregistry.azurecr.io/techtalksproducer:azurecontainerapp `
+        --registry-server ngacrregistry.azurecr.io `
+        --target-port 80 `
+        --ingress 'external' `
+        --enable-dapr `
+        --dapr-app-id rabbitmq-producer `
+        --dapr-app-port 80 `
+        --min-replicas 1 `
+        --max-replicas 3
   
     
-#--query configuration.ingress.fqdn
+    #--query configuration.ingress.fqdn
     
-Write-Host "Successfully created Azure Container App for Producer "  -ForegroundColor Yellow
+    Write-Host "Successfully created Azure Container App for Producer "  -ForegroundColor Yellow
+}
+else {
+    Write-Host "Azure Container App for Producer already exists"  -ForegroundColor Yellow
+}
 
-# Create Azure Container App for TechTalks Producer application
-Write-Host "Creating Azure Container App for Consumer "  -ForegroundColor Yellow
-
-az containerapp create `
-    --environment $environmentName `
+$consumerApp = az containerapp show `
     --resource-group $resourceGroupName `
     --name techtalks-consumer `
-    --image ngacrregistry.azurecr.io/techtalksconsumer:azurecontainerapp `
-    --registry-server ngacrregistry.azurecr.io `
-    --target-port 80 `
-    --ingress 'internal' `
-    --enable-dapr `
-    --dapr-app-id rabbitmq-consumer `
-    --dapr-app-port 80 `
-    --min-replicas 1 `
+    --query name | ConvertFrom-Json
+
+$consumerAppExists = $consumerApp.Length -gt 0
+
+if ($consumerAppExists -eq $false) {
+    # Create Azure Container App for TechTalks Producer application
+    Write-Host "Creating Azure Container App for Consumer "  -ForegroundColor Yellow
+
+    az containerapp create `
+        --environment $environmentName `
+        --resource-group $resourceGroupName `
+        --name techtalks-consumer `
+        --image ngacrregistry.azurecr.io/techtalksconsumer:azurecontainerapp `
+        --registry-server ngacrregistry.azurecr.io `
+        --target-port 80 `
+        --ingress 'internal' `
+        --enable-dapr `
+        --dapr-app-id rabbitmq-consumer `
+        --dapr-app-port 80 `
+        --min-replicas 1 `
   
     
-#--query configuration.ingress.fqdn
+    #--query configuration.ingress.fqdn
     
-Write-Host "Successfully created Azure Container App for Consumer "  -ForegroundColor Yellow
+    Write-Host "Successfully created Azure Container App for Consumer "  -ForegroundColor Yellow
+}
+else {
+    Write-Host "Azure Container App for Consumer already exists"  -ForegroundColor Yellow
+}  
 
-##Create a new secret named 'rabbitmq-host' in backend processer container app
-az containerapp secret set `
-    --name techtalks-consumer `
+$consumerAppSecret = az containerapp secret show `
     --resource-group $resourceGroupName `
-    --secrets "rabbitmq-host=amqp://user:tCUN6UizuwTZ@20.24.98.54:5672/"
+    --name techtalks-consumer `
+    --secret-name rabbitmq-host `
+    --query name | ConvertFrom-Json
 
+$consumerAppSecretExists = $consumerAppSecret.Length -gt 0
+
+if ($consumerAppSecretExists -eq $false) {
+
+    Write-Host "Creating Azure Container App Secret named rabbitmq-host"  -ForegroundColor Yellow
+
+    ##Create a new secret named 'rabbitmq-host' in backend processer container app
+    az containerapp secret set `
+        --name techtalks-consumer `
+        --resource-group $resourceGroupName `
+        --secrets "rabbitmq-host=amqp://user:tCUN6UizuwTZ@20.24.98.54:5672/"
+
+}
+else {
+    Write-Host "Azure Container App Secret already exists"  -ForegroundColor Yellow
+}
 # define KEDA autoscaler
 
 az containerapp update `
